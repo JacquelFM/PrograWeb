9041a53f85522668c39c510ff00f18900f9ecfaf
JacquelineLK119 <jflores43@ucol.mx>
2020-05-12 22:23:04 -0500

    body-parser

--- Jueves_07_05_2020/App.js
+++ Jueves_07_05_2020/App.js
@@ -1,14 +1,38 @@
 //Inyección de dependencias.
 const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
 const port = 3000;
+
+//Codificador del URL.
+var urlencodedParser = bodyParser.urlencoded({ extended: false });
+var jsonParser = bodyParser.json();
 
 //Mapeo.
 app.use('/assets', express.static(__dirname + '/public')); //Aquí estarán nuestros contenidos estáticos.
 app.set('view engine', 'ejs');
 
+app.get('/', (req, res) => {
+    res.render('personData');
+});
+
 app.get('/person/:id', (req, res) => {
-    res.render('index', { ID: req.params.id });
+    res.render('person', {
+        ID: req.params.id,
+        Qstr: req.query.Qstr
+    });
+});
+
+app.post('/person', urlencodedParser, (req, res) => {
+    res.send('Thanks');
+    console.log(req.body.firstname);
+    console.log(req.body.lastname);
+});
+
+app.post('/personJson', jsonParser, (req, res) => {
+    res.send('Thanks from jsonParser');
+    console.log(req.body.firstname);
+    console.log(req.body.lastname);
 });
 
 app.listen(port, () => {
