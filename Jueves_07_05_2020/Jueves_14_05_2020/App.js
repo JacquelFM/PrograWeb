37c709b90dab8eceb6881c8cf96b0e4dd26cb268
JacquelineLK119 <jflores43@ucol.mx>
2020-05-21 21:22:21 -0500

    Mongoose

--- Jueves_07_05_2020/Jueves_14_05_2020/App.js
+++ Jueves_07_05_2020/Jueves_14_05_2020/App.js
@@ -1,21 +1,55 @@
 //Inyección de dependencias.
 const mongoose = require('mongoose');
 const express = require('express');
+const app = express();
+const port = 3000;
 
 //Hacemos la conexión.
-mongoose.connect('mongodb+srv://admin:admin@beducluster-eiuzk.mongodb.net/test?retryWrites=true&w=majority');
+mongoose.connect('mongodb+srv://JacquelineFlores:Jacqueline119@clusterjacqueline-bnkn2.mongodb.net/test?retryWrites=true&w=majority');
 
 //Mapear algo lógico con algo físico.
 var Schema = mongoose.Schema;
 
-var userSchema = new Schema({
-    userName: String,
-    password: String
+var gameSchema = new Schema({
+    id: String,
+    nameGame: String,
+    platform: String,
+    rating: Number,
+    genders: String
 });
 
-//User
-var user = mongoose.model('User', userSchema);
+app.get('/game', (req, res) => {
+    var game = mongoose.model('game', gameSchema);
 
+    game.find({ nameGame: 'Minecraft' }, function(err, data) {
+        // doc is a Document
+        if (err) {
+            console.log('Lo sentimos. Ocurrió un error.');
+        } else {
+            // res.send(data);
+            res.send(data);
+        }
+    });
+});
+
+app.listen(port, () => {
+    console.log(`Escuchando en el puerto ${port}`)
+});
+
+//Busca todos
+/*
+user.find({ userName: 'Oscar' }, function(err, data) {
+    // doc is a Document
+    if (err) {
+        console.log('Lo sentimos. Ocurrió un error.');
+
+    } else {
+        // res.send(data);
+        console.log(data);
+    }
+});*/
+
+/*
 var myUser = user({
     userName: 'Jacqueline',
     password: "Flores"
@@ -27,4 +61,4 @@ myUser.save((err) => {
     } else {
         console.log('Todo en orden, crack.');
     }
-});
+});*/
