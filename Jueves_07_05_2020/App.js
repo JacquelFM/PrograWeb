9a55d9f62ae526f36edf08f5acd30fd1f2a70fb3
JacquelineLK119 <jflores43@ucol.mx>
2020-05-26 19:50:14 -0500

    21Mayo

--- Jueves_07_05_2020/App.js
+++ Jueves_07_05_2020/App.js
@@ -1,15 +1,21 @@
 //Inyección de dependencias.
 const express = require('express');
 const bodyParser = require('body-parser');
-
+const mongoose = require('mongoose');
 const app = express();
 const port = 3000;
 
-//Codificador del URL.
 var urlencodedParser = bodyParser.urlencoded({ extended: false });
 var jsonParser = bodyParser.json();
 
-//Mapeo.
+mongoose.connect('mongodb+srv://admin:admin@beducluster-eiuzk.mongodb.net/test?retryWrites=true&w=majority');
+
+var Schema = mongoose.Schema;
+var userSchema = new Schema({
+    userName: String,
+    password: String
+});
+
 app.use('/assets', express.static(__dirname + '/public')); //Aquí estarán nuestros contenidos estáticos.
 app.set('view engine', 'ejs');
 
@@ -25,16 +31,57 @@ app.get('/person/:id', (req, res) => {
 });
 
 app.post('/person', urlencodedParser, (req, res) => {
-    //res.send('Thanks');
-    console.log(req.body.userName);
+    var user = mongoose.model('User', userSchema);
 
-    //console.log(req.body.firstname);
-    //console.log(req.body.lastname);
+    user.find({ userName: req.body.userName }, function(err, data) {
+        if (err) console.log('Hubo un error');
+        else {
+            if (data.length > 0) res.render('results', { data });
+            else res.send('No hay coincidencias para el criterio de búsqueda');
+        }
+    });
 });
+
+/*
+app.post('/person', urlencodedParser, (req, res) => {
+    // res.send('Thanks');
+    console.log(req.body.userName);
+    // console.log(req.body.lastname);
+    //Aquí empieza la busqueda
+    var user = mongoose.model('User', userSchema);
+
+    user.find({ userName: req.body.userName }, function (err, data) {
+        // doc is a Document
+        if (err) {
+            console.log('Hubo un error');
+        } else {
+            if (data.length > 0) {
+                // console.log(data);
+                res.render('results', { data });
+                //res.send(data);
+                // console.log(doc);
+            } else { res.send('no hay coincidencias para el criterio de busqueda'); }
+        }
+    });
+});
+*/
+/*
+app.get('/person', (req, res) => {
+    var user = mongoose.model('User', userSchema);
+
+    user.find({ userName: req.body.userName }, function(err, data) {
+        if (err) {
+            console.log('Lo sentimos. Ocurrió un error.');
+        } else {
+            res.send(data);
+        }
+    });
+});
+*/
 
 app.post('/personJson', jsonParser, (req, res) => {
     res.send('Thanks from jsonParser.');
-    console.log(req.body.firstname);
+    console.log(req.body.userName);
     console.log(req.body.lastname);
 });
 
