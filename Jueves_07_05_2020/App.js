2e547b7b26585a98ec1fe9d76abe940e234ef68c
JacquelineLK119 <jflores43@ucol.mx>
2020-05-26 21:43:49 -0500

    26Mayo

--- Jueves_07_05_2020/App.js
+++ Jueves_07_05_2020/App.js
@@ -19,6 +19,21 @@ var userSchema = new Schema({
 app.use('/assets', express.static(__dirname + '/public')); //Aquí estarán nuestros contenidos estáticos.
 app.set('view engine', 'ejs');
 
+/*
+app.get('/', (req, res) => {
+    res.render('index');
+});
+
+app.get('/nsert', (req, res) => {
+    res.render('insert');
+});
+
+app.get('/search', (req, res) => {
+    res.render('personData');
+});
+
+*/
+
 app.get('/', (req, res) => {
     res.render('personData');
 });
@@ -36,8 +51,11 @@ app.post('/person', urlencodedParser, (req, res) => {
     user.find({ userName: req.body.userName }, function(err, data) {
         if (err) console.log('Hubo un error');
         else {
-            if (data.length > 0) res.render('results', { data });
-            else res.send('No hay coincidencias para el criterio de búsqueda');
+            if (data.length > 0) {
+                res.render('results', { data });
+            } else {
+                res.send('No hay coincidencias para el criterio de búsqueda');
+            }
         }
     });
 });
